53a6b301515bdff77f4d578eba9d39e9661ccfa3
Patrick <patrick@gbrco.net>
2016-11-24 15:52:09 +0800

    commit ver 2

---
+++ src/test1.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+/**
+ * @author Patrick
+ *
+ */
+public class test1 {
+
+	 public static void main(String []args) {
+	        System.out.println("Hello World 1");
+	        System.out.println("\nHello World 2");
+	    }
+
+}
