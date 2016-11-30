4472840f0a695a04558ec0188dab99a3a75f8f0c
Patrick Hsueh <patrick@gbrco.net>
2016-11-30 18:05:12 +0800

    added new sample.

---
+++ src/noPkg/Test2.java
@@ -0,0 +1,18 @@
+package noPkg;
+
+public class Test2 {
+
+	//public static void main(String[] args) {
+	//}
+	
+    public static int gvar;
+    public static void say(String s) {
+        int x = 10;
+        System.out.print(s+x);
+    }
+    public static void main(String[] argv) {
+        float y = 0;
+        say("Hello, world\n");
+    }
+
+}
