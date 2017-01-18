8a2f7e1c41fddbc4e72b3cb5dae2bfa9a5fde405
Patrick Hsueh <patrick@gbrco.net>
2017-01-18 11:29:06 +0800

    test

---
+++ src/myPkg11/RunCAD.java
@@ -0,0 +1,23 @@
+package myPkg11;
+
+public class RunCAD {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 111");
+		foo();
+
+	}
+
+	
+	static int foo() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 222");
+
+		return 0;
+	}
+
+}
+
+
