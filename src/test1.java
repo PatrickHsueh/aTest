baccb57c4ee3a83c88775d8ff5c90cad09e824e1
PatrickHsueh <patrick@gbrco.net>
2017-12-13 12:04:04 +0800

    update from git, v3

--- src/test1.java
+++ src/test1.java
@@ -11,6 +11,8 @@ public class test1 {
 	 public static void main(String []args) {
 	        System.out.println("Hello World 1");
 	        System.out.println("\nHello World 2");
+		 
+		System.out.println("\nHello World v3");
 	    }
 
 }
